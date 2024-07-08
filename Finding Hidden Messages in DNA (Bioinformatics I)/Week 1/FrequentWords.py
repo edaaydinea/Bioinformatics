58700ced2c8818e3f416761bad598eddd41fd99a
Eda AYDIN <edaaydin.ea@outlook.com>
2024-07-08 23:07:58 +0300

    refactor: Improve efficiency and readability of FrequentWords function

--- Finding Hidden Messages in DNA (Bioinformatics I)/Week 1/FrequentWords.py
+++ Finding Hidden Messages in DNA (Bioinformatics I)/Week 1/FrequentWords.py
@@ -1,34 +1,26 @@
-# Frequent Words Problem: Find the most frequent k-mers in a string.
-#      Input: A string Text and an integer k.
-#      Output: All most frequent k-mers in Text.
+def FrequentWords(Text, k):
+    kmer_count = {}
+    max_count = 0
+    
+    for i in range(len(Text) - k + 1):
+        kmer = Text[i:i+k]
+        if kmer in kmer_count:
+            kmer_count[kmer] += 1
+        else:
+            kmer_count[kmer] = 1
+        if kmer_count[kmer] > max_count:
+            max_count = kmer_count[kmer]
+   
+    #Find the most frequent k-mers
+    most_frequent_kmers = [kmer for kmer in kmer_count if kmer_count[kmer] == max_count]
+    
+    return ' '.join(most_frequent_kmers)
 
-# Sample Input:
-#      ACGTTGCATGTCGCATGATGCATGAGAGCT
-#      4
+with open('dataset_30272_13.txt', 'r') as file:
+	text = file.readline().strip()
+	k = int(file.readline().strip())
+ 
+result = FrequentWords(text, k)
 
-# Sample Output:
-#      CATG GCAT
+print(result)
 
-sequence = input("What is the genomic sequence? ")
-k = int(input("What is the length of k-mer? "))
-
-DNA = str(sequence.upper())
-
-counts = {}
-
-for i in range(0, len(DNA) - k + 1):
-	kmer = DNA[i:i + k]
-	if kmer in counts:
-		counts[kmer] += 1
-	else:
-		counts[kmer] = 1
-
-frequent = max(counts.values())
-
-ans = []
-for k in counts:
-	if counts[k] == frequent:
-		print(k)
-		ans.append(k)
-		
-print(ans)
